327c3ed5223ff8dec35e1ff724b65bc433db9d3d
sumon-ray <raysumon220020@gmail.com>
2025-04-11 23:59:47 +0600

    filtering added based on field

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -2,26 +2,46 @@ import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 const getAdminsFromDB = async (params: any) => {
-  // console.log({ params });
+  const {searchTerm, ...filteredData} = await params
+  console.log({ filteredData });
   const andConditions: Prisma.AdminWhereInput[] = [];
   if (params.searchTerm) {
     andConditions.push({
-      OR: [
-        {
-          name: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
+      OR: ["name", "email"].map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
         },
-        {
-          email: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
+      })),
     });
   }
+
+  // specific fields er upor filtering
+  if (Object.keys(filteredData).length>0) {
+    andConditions.push({
+      AND: Object.keys(filteredData).map(key=>({
+        [key]: {
+          equals: filteredData[key]
+        }
+      }))
+    })
+  }
+  //
+  // [
+  //   {
+  //     name: {
+  //       contains: params.searchTerm,
+  //       mode: "insensitive",
+  //     },
+  //   },
+  //   {
+  //     email: {
+  //       contains: params.searchTerm,
+  //       mode: "insensitive",
+  //     },
+  //   },
+  // ],
+
   const whereConditions: Prisma.AdminWhereInput = { AND: andConditions };
   const result = await prisma.admin.findMany({
     where: whereConditions,
